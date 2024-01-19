8e956991dcc927e352ebe4c97f0e1672b350f802
Edwin Cardenas <eacardenase@unal.edu.co>
2024-01-19 10:53:11 -0500

    feat: add tasks screen scafold

--- lib/screens/tasks.dart
+++ lib/screens/tasks.dart
@@ -6,16 +6,29 @@ class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.yellow.shade200,
       appBar: AppBar(
-        title: const Text(
+        title: Text(
           "R5 ToDo",
+          style: TextStyle(
+            letterSpacing: 1,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey.shade700,
+          ),
         ),
         backgroundColor: Colors.yellow,
+        elevation: 0,
       ),
       body: const Center(
         child: Text(
           'My ToDo',
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.yellow,
+        shape: const CircleBorder(),
+        child: const Icon(Icons.add),
       ),
     );
   }
